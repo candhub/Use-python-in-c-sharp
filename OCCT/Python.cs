9b21756375a29b976747c7e8eef7ab000dd3bb9a
Ben <develop.cand@gmail.com>
2022-11-08 15:12:35 +0900

    Test

--- OCCT/Python.cs
+++ OCCT/Python.cs
@@ -2,15 +2,18 @@ namespace CD;
 
 public static class Python
 {
+    private static readonly string path = @"/Users/ben/Desktop/OCCT/OCCT/test.py";
+
     public static string Get_box_svg()
     {
         var engine = IronPython.Hosting.Python.CreateEngine();
         var scope = engine.CreateScope();
 
-        var source = engine.CreateScriptSourceFromFile(@"/Users/ben/Desktop/OCCT/OCCT/test.py");
+        var source = engine.CreateScriptSourceFromFile(path);
         source.Execute(scope);
 
-        var make_box = scope.GetVariable<Func<string>>("Make_box");
+        // var make_box = scope.GetVariable<Func<string>>("Make_box");
+        var make_box = scope.GetVariable<Func<string>>("Say_hello");
         return make_box();
     }
 }
--- OCCT/test.py
+++ OCCT/test.py
@@ -1,5 +1,8 @@
-import cadquery
+# import cadquery
 
 
-def Make_box():
-    return cadquery.Workplane("XY").box(1, 1, 1).toSvg()
+# def Make_box():
+#     return cadquery.Workplane("XY").box(1, 1, 1).toSvg()
+
+def Say_hello():
+    return "hello"
